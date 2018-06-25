800bae0b310927d911cade1c0e4e2186fa4966b5
raina <rainaoo2017@gmail.com>
2018-06-25 12:12:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,9 @@
 //define functions here
 
-$(document).ready(function(){
-
-function getIt(){
+function getIt() {
   $('p').on('click', function() {
     alert('hey!')
-  });
+  })
 }
 
 function frameIt() {
@@ -13,6 +11,7 @@ function frameIt() {
     $('img').addClass('tasty')
   })
 }
+
 function pressIt() {
   $('#typing').on('keydown', function(e) {
     // 'g' corresponds to 71
@@ -21,10 +20,15 @@ function pressIt() {
     }
   })
 }
+
 function submitIt() {
   $('form').on('submit', function(e) {
     alert('Your form is going to be submitted now.')
-  });
+  })
 }
+
+$(document).ready(function(){
+
+// call functions here
 
 });
